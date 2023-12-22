6fb6a20b36105c022c3da7e7549df997a145e6bd
vyacheslav.butkevich <vyacheslav.butkevich@gmail.com>
2023-12-22 22:37:31 +0200

    modalform.js

---
+++ scripts/modalform.js
@@ -0,0 +1,37 @@
+
+// var herobutton = document.querySelector("#hero-button");
+// console.log(herobutton);
+var modalWindow_Open_Btn = document.getElementById("hero-button");
+console.log(modalWindow_Open_Btn);
+modalWindow_Open_Btn.addEventListener("click", function () {
+    ModalWindow_Open()
+});
+
+var modalWindow_Close_Btn = document.getElementById("modal-window-close");
+modalWindow_Close_Btn.addEventListener("click", function () {
+    ModalWindow_Close()
+});
+
+var modalWindow_Send_Btn = document.getElementById("modal-form-send-button");
+modalWindow_Send_Btn.addEventListener("click", function () {
+    ModalWindow_Send()
+});
+
+function ModalWindow_Open() {
+    var modalForm = document.querySelector("#modal-form");
+    modalForm.classList.remove('is-hidden');
+    modalForm.classList.add('is-open');
+}
+
+function ModalWindow_Close() {
+    var modalForm = document.querySelector("#modal-form");
+    modalForm.classList.remove('is-open');
+    modalForm.classList.add('is-hidden');
+}
+
+function ModalWindow_Send() {
+    console.log("Send.click()");
+    // console.log(event);
+    // console.log(event.Target);
+
+}
--- scripts/version.js
+++ scripts/version.js
@@ -4,7 +4,7 @@ var version = '1.002';//hero:width:1440px;
 var version = '1.005';//lesson:module.#5;
 var date = '[2023.12.20]v.1.006:lesson:module.#5;';
 var curentDate = new Date().toDateString('yyyy.MM.dd');
-var version = '[2023.12.22]v.1.007:lesson:module.#5;';
+var version = '[2023.12.22]v.1.008:lesson:module.#5.10;';
 // document.getElementById('version').innerHTML = ' module.#' + module + ' [' + date + ']' + 'v:' + version;
 document.getElementById('version').innerHTML = ' module.#' + module + ' | ' + version;
 
