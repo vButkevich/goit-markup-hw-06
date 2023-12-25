2da154c3c4c0d47de17d86c14a16830ff684811f
vyacheslav.butkevich <vyacheslav.butkevich@gmail.com>
2023-12-25 17:03:08 +0200

    version

--- scripts/version.js
+++ scripts/version.js
@@ -8,5 +8,4 @@ let date = '[2023.12.20]v.1.006:lesson:module.#5;';
 let curentDate = new Date().toDateString('yyyy.MM.dd');
 let version = '[2023.12.25]v.1.009:lesson:module.#5.10;';
 // document.getElementById('version').innerHTML = ' module.#' + module + ' [' + date + ']' + 'v:' + version;
-document.getElementById('version').innerHTML =
-    ' module.#' + module + ' | ' + version;
+document.getElementById('version').innerHTML = ` module.#${module}|${version}`;
