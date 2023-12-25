8064518255c2c26d9f1b955e6993f376608acc8d
vyacheslav.butkevich <vyacheslav.butkevich@gmail.com>
2023-12-25 16:50:06 +0200

    05

--- scripts/modalform.js
+++ scripts/modalform.js
@@ -2,36 +2,39 @@
 
 // let herobutton = document.querySelector("#hero-button");
 // console.log(herobutton);
+//document.querySelector()
+//let divCollection = document.querySelectorAll('div');
+
 let modalWindow_Open_Btn = document.getElementById('hero-button');
 console.log(modalWindow_Open_Btn);
 modalWindow_Open_Btn.addEventListener('click', function () {
-  ModalWindow_Open();
+    ModalWindow_Open();
 });
 
 let modalWindow_Close_Btn = document.getElementById('modal-window-close');
 modalWindow_Close_Btn.addEventListener('click', function () {
-  ModalWindow_Close();
+    ModalWindow_Close();
 });
 
 let modalWindow_Send_Btn = document.getElementById('modal-form-send-button');
 modalWindow_Send_Btn.addEventListener('click', function () {
-  ModalWindow_Send();
+    ModalWindow_Send();
 });
 
 function ModalWindow_Open() {
-  let modalForm = document.querySelector('#modal-form');
-  modalForm.classList.remove('is-hidden');
-  modalForm.classList.add('is-open');
+    let modalForm = document.querySelector('#modal-form');
+    modalForm.classList.remove('is-hidden');
+    modalForm.classList.add('is-open');
 }
 
 function ModalWindow_Close() {
-  let modalForm = document.querySelector('#modal-form');
-  modalForm.classList.remove('is-open');
-  modalForm.classList.add('is-hidden');
+    let modalForm = document.querySelector('#modal-form');
+    modalForm.classList.remove('is-open');
+    modalForm.classList.add('is-hidden');
 }
 
 function ModalWindow_Send() {
-  console.log('Send.click()');
-  // console.log(event);
-  // console.log(event.Target);
+    console.log('Send.click()');
+    // console.log(event);
+    // console.log(event.Target);
 }
--- scripts/version.js
+++ scripts/version.js
@@ -6,7 +6,7 @@ let module = '6';
 // let version = '1.005'; //lesson:module.#5;
 let date = '[2023.12.20]v.1.006:lesson:module.#5;';
 let curentDate = new Date().toDateString('yyyy.MM.dd');
-let version = '[2023.12.22]v.1.008:lesson:module.#5.10;';
+let version = '[2023.12.25]v.1.009:lesson:module.#5.10;';
 // document.getElementById('version').innerHTML = ' module.#' + module + ' [' + date + ']' + 'v:' + version;
 document.getElementById('version').innerHTML =
-  ' module.#' + module + ' | ' + version;
+    ' module.#' + module + ' | ' + version;
