ab737e4fbc70c3fb53fe367d1c497b4844cc2e13
vyacheslav.butkevich <vyacheslav.butkevich@gmail.com>
2023-12-29 01:42:08 +0200

    mobile-menu

---
+++ scripts/mobilemenu.js
@@ -0,0 +1,24 @@
+'use strict'
+{
+    let menu = document.querySelector('.mobile-menu');
+
+    let menuBtnOpen = document.querySelector('.mobile-menu-open');
+    menuBtnOpen.addEventListener('click', function () {
+        mobileMenuToggle();
+    })
+    let menuBtnClose = document.querySelector('.mobile-menu-close');
+    menuBtnClose.addEventListener('click', function () {
+        mobileMenuToggle();
+    })
+
+    function mobileMenuToggle() {
+        menu.classList.toggle('active');
+    }
+
+
+
+    function mobileMenuOpenClose() {
+        menu.classList.toggle('active');
+        // menu.classList.add('is-open');
+    }
+}
