09e91ebfe7a9cdb315f76d7a976f92f91e6bea67
vyacheslav.butkevich <vyacheslav.butkevich@gmail.com>
2023-12-28 22:21:42 +0200

    adaptive

--- scripts/version.js
+++ scripts/version.js
@@ -1,5 +1,7 @@
 'use strict';
-
+function showVersion(version) {
+    document.getElementById('version').innerHTML = `module.#${module}|${version}`;
+}
 let module = '6';
 // let version = '1.001';
 // let version = '1.002'; //hero:width:1440px;
@@ -7,5 +9,5 @@ let module = '6';
 let date = '[2023.12.20]v.1.006:lesson:module.#5;';
 let curentDate = new Date().toDateString('yyyy.MM.dd');
 let version = '[2023.12.25]v.1.009:lesson:module.#5.10;';
-// document.getElementById('version').innerHTML = ' module.#' + module + ' [' + date + ']' + 'v:' + version;
-document.getElementById('version').innerHTML = ` module.#${module}|${version}`;
+version = '[2023.12.28]v.1.011:lesson:module.#5.11.adaptive;';
+showVersion(version);
