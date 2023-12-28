7d384c1991b472ed7d6aa53ee5bfcb3449a2e381
vyacheslav.butkevich <vyacheslav.butkevich@gmail.com>
2023-12-29 01:41:44 +0200

    mobile-menu

--- scripts/version.js
+++ scripts/version.js
@@ -1,13 +1,13 @@
 'use strict';
-function showVersion(version) {
-    document.getElementById('version').innerHTML = `module.#${module}|${version}`;
+{
+    let module = '6';
+
+    function showVersion(version) {
+        document.getElementById('version').innerHTML = `module.#${module}|${version}`;
+    }
+    let version;
+    version = `[2023.12.25]v.1.009:lesson:10;`;
+    version = `[2023.12.28]v.1.011:lesson:11.adaptive;`;
+
+    showVersion(version);
 }
-let module = '6';
-// let version = '1.001';
-// let version = '1.002'; //hero:width:1440px;
-// let version = '1.005'; //lesson:module.#5;
-let date = '[2023.12.20]v.1.006:lesson:module.#5;';
-let curentDate = new Date().toDateString('yyyy.MM.dd');
-let version = '[2023.12.25]v.1.009:lesson:module.#5.10;';
-version = '[2023.12.28]v.1.011:lesson:module.#5.11.adaptive;';
-showVersion(version);
